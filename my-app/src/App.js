c3a9bb18746417d79f247eee08d6ddd59075de2f
BRp99 <124196319+BRp99@users.noreply.github.com>
2024-09-17 08:42:36 +0100

    improve code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,43 +5,121 @@ import { useState, useRef, useEffect } from "react"
 //e também por este site para dupla verificação: https://laendercode.net/de/
 
 const europeanCountrys = [
-  { id: 1, nome: "Áustria", codigoISOAlpha2: "AT", pattern: "U[0-9]{8}" }, //começa com 'U' e 8 dígitos
-  { id: 2, nome: "Bélgica", codigoISOAlpha2: "BE", pattern: "0[0-9]{9}" }, //começa com 0 e 9 dígitos
-  { id: 3, nome: "Bulgária", codigoISOAlpha2: "BG", pattern: "[0-9]{9,10}" }, //pode ter 9 ou 10 dígitos
-  { id: 4, nome: "Croácia", codigoISOAlpha2: "HR", pattern: "[0-9]{11}" },
-  { id: 5, nome: "Chipre", codigoISOAlpha2: "CY", pattern: "[0-9]{8}L" }, //começa com 8 dígitos e a letra 'L'
-  { id: 6, nome: "República Checa", codigoISOAlpha2: "CZ", pattern: "[0-9]{8,10}" },
-  { id: 7, nome: "Dinamarca", codigoISOAlpha2: "DK", pattern: "[0-9]{8}" },
-  { id: 8, nome: "Eslováquia", codigoISOAlpha2: "SK", pattern: "[0-9]{10}" },
-  { id: 9, nome: "Eslovênia", codigoISOAlpha2: "SI", pattern: "[0-9]{8}" },
-  { id: 10, nome: "Espanha", codigoISOAlpha2: "ES", pattern: "[0-9A-Z][0-9]{7}[0-9A-Z]" }, //começa com um caractere alfanumérico, depois 7 dígitos e um caractere alfanumérico
-  { id: 11, nome: "Estónia", codigoISOAlpha2: "EE", pattern: "[0-9]{9}" },
-  { id: 12, nome: "Finlândia", codigoISOAlpha2: "FI", pattern: "[0-9]{8}" },
-  { id: 13, nome: "França", codigoISOAlpha2: "FR", pattern: "[0-9A-Z]{2}[0-9]{9}" }, //começa com 2 caracteres alfanuméricos e depois 9 dígitos
-  { id: 14, nome: "Grécia", codigoISOAlpha2: "GR", pattern: "[0-9]{9}" },
-  { id: 15, nome: "Hungria", codigoISOAlpha2: "HU", pattern: "[0-9]{8}" },
-  { id: 16, nome: "Irlanda", codigoISOAlpha2: "IE", pattern: "[0-9]S[0-9]{5}L" }, //começa por um dígito, "S", 5 dígitos e "L"
-  { id: 17, nome: "Itália", codigoISOAlpha2: "IT", pattern: "[0-9]{11}" },
-  { id: 18, nome: "Letônia", codigoISOAlpha2: "LV", pattern: "[0-9]{11}" },
-  { id: 19, nome: "Lituânia", codigoISOAlpha2: "LT", pattern: "[0-9]{9}|[0-9]{12}" },
-  { id: 20, nome: "Luxemburgo", codigoISOAlpha2: "LU", pattern: "[0-9]{8}" },
-  { id: 21, nome: "Malta", codigoISOAlpha2: "MT", pattern: "[0-9]{8}" },
-  { id: 22, nome: "Países Baixos", codigoISOAlpha2: "NL", pattern: "[0-9]{9}B[0-9]{2}" }, //começa por 9 dígitos, "B" e 2 dígitos
-  { id: 23, nome: "Polônia", codigoISOAlpha2: "PL", pattern: "[0-9]{10}" },
-  { id: 24, nome: "Portugal", codigoISOAlpha2: "PT", pattern: "[0-9]{9}" },
-  { id: 25, nome: "Romênia", codigoISOAlpha2: "RO", pattern: "[0-9]{2,10}" },
-  { id: 26, nome: "Suécia", codigoISOAlpha2: "SE", pattern: "[0-9]{12}" },
-  { id: 27, nome: "Alemanha", codigoISOAlpha2: "DE", pattern: "[0-9]{9}" },
+  {
+    id: 1,
+    nome: "Áustria",
+    codigoISOAlpha2: "AT",
+    pattern: "U[0-9]{8}",
+    exampleVAT: "U12345678",
+    descricaoExampleVAT: 'Tem de começar com a letra "U", seguida de 8 dígitos.',
+  },
+  {
+    id: 2,
+    nome: "Bélgica",
+    codigoISOAlpha2: "BE",
+    pattern: "0[0-9]{9}",
+    exampleVAT: "0123456789",
+    descricaoExampleVAT: 'Tem de começar com "0" e ser seguido por 9 dígitos.',
+  },
+  {
+    id: 3,
+    nome: "Bulgária",
+    codigoISOAlpha2: "BG",
+    pattern: "[0-9]{9,10}",
+    exampleVAT: "123456789",
+    descricaoExampleVAT: "Pode ter 9 ou 10 dígitos.",
+  },
+  { id: 4, nome: "Croácia", codigoISOAlpha2: "HR", pattern: "[0-9]{11}", exampleVAT: "12345678901", descricaoExampleVAT: "Tem de ter 11 dígitos." },
+  {
+    id: 5,
+    nome: "Chipre",
+    codigoISOAlpha2: "CY",
+    pattern: "[0-9]{8}L",
+    exampleVAT: "12345678L",
+    descricaoExampleVAT: 'Tem de começar com 8 dígitos e terminar com a letra "L".',
+  },
+  {
+    id: 6,
+    nome: "República Checa",
+    codigoISOAlpha2: "CZ",
+    pattern: "[0-9]{8,10}",
+    exampleVAT: "12345678",
+    descricaoExampleVAT: "Pode ter entre 8 e 10 dígitos.",
+  },
+  { id: 7, nome: "Dinamarca", codigoISOAlpha2: "DK", pattern: "[0-9]{8}", exampleVAT: "12345678", descricaoExampleVAT: "Tem de ter 8 dígitos." },
+  { id: 8, nome: "Eslováquia", codigoISOAlpha2: "SK", pattern: "[0-9]{10}", exampleVAT: "1234567890", descricaoExampleVAT: "Tem de ter 10 dígitos." },
+  { id: 9, nome: "Eslovênia", codigoISOAlpha2: "SI", pattern: "[0-9]{8}", exampleVAT: "12345678", descricaoExampleVAT: "Tem de ter 8 dígitos." },
+  {
+    id: 10,
+    nome: "Espanha",
+    codigoISOAlpha2: "ES",
+    pattern: "[0-9A-Z][0-9]{7}[0-9A-Z]",
+    exampleVAT: "X12345678",
+    descricaoExampleVAT: "Começa com um caractere alfanumérico, seguido por 7 dígitos e um caractere alfanumérico.",
+  },
+  { id: 11, nome: "Estónia", codigoISOAlpha2: "EE", pattern: "[0-9]{9}", exampleVAT: "123456789", descricaoExampleVAT: "Tem de ter 9 dígitos." },
+  { id: 12, nome: "Finlândia", codigoISOAlpha2: "FI", pattern: "[0-9]{8}", exampleVAT: "12345678", descricaoExampleVAT: "Tem de ter 8 dígitos." },
+  {
+    id: 13,
+    nome: "França",
+    codigoISOAlpha2: "FR",
+    pattern: "[0-9A-Z]{2}[0-9]{9}",
+    exampleVAT: "AB123456789",
+    descricaoExampleVAT: "Começa com 2 caracteres alfanuméricos, seguidos de 9 dígitos.",
+  },
+  { id: 14, nome: "Grécia", codigoISOAlpha2: "GR", pattern: "[0-9]{9}", exampleVAT: "123456789", descricaoExampleVAT: "Tem de ter 9 dígitos." },
+  { id: 15, nome: "Hungria", codigoISOAlpha2: "HU", pattern: "[0-9]{8}", exampleVAT: "12345678", descricaoExampleVAT: "Tem de ter 8 dígitos." },
+  {
+    id: 16,
+    nome: "Irlanda",
+    codigoISOAlpha2: "IE",
+    pattern: "[0-9]S[0-9]{5}L",
+    exampleVAT: "1S23456L",
+    descricaoExampleVAT: 'Começa com um dígito, seguido pela letra "S", 5 dígitos e termina com a letra "L".',
+  },
+  { id: 17, nome: "Itália", codigoISOAlpha2: "IT", pattern: "[0-9]{11}", exampleVAT: "12345678901", descricaoExampleVAT: "Tem de ter 11 dígitos." },
+  { id: 18, nome: "Letônia", codigoISOAlpha2: "LV", pattern: "[0-9]{11}", exampleVAT: "12345678901", descricaoExampleVAT: "Tem de ter 11 dígitos." },
+  {
+    id: 19,
+    nome: "Lituânia",
+    codigoISOAlpha2: "LT",
+    pattern: "[0-9]{9}|[0-9]{12}",
+    exampleVAT: "123456789",
+    descricaoExampleVAT: "Pode ter 9 ou 12 dígitos.",
+  },
+  { id: 20, nome: "Luxemburgo", codigoISOAlpha2: "LU", pattern: "[0-9]{8}", exampleVAT: "12345678", descricaoExampleVAT: "Tem de ter 8 dígitos." },
+  { id: 21, nome: "Malta", codigoISOAlpha2: "MT", pattern: "[0-9]{8}", exampleVAT: "12345678", descricaoExampleVAT: "Tem de ter 8 dígitos." },
+  {
+    id: 22,
+    nome: "Países Baixos",
+    codigoISOAlpha2: "NL",
+    pattern: "[0-9]{9}B[0-9]{2}",
+    exampleVAT: "123456789B01",
+    descricaoExampleVAT: 'Começa com 9 dígitos, seguido da letra "B" e 2 dígitos.',
+  },
+  { id: 23, nome: "Polônia", codigoISOAlpha2: "PL", pattern: "[0-9]{10}", exampleVAT: "1234567890", descricaoExampleVAT: "Tem de ter 10 dígitos." },
+  { id: 24, nome: "Portugal", codigoISOAlpha2: "PT", pattern: "[0-9]{9}", exampleVAT: "249888776", descricaoExampleVAT: "Tem de ter 9 dígitos." },
+  {
+    id: 25,
+    nome: "Romênia",
+    codigoISOAlpha2: "RO",
+    pattern: "[0-9]{2,10}",
+    exampleVAT: "1234567890",
+    descricaoExampleVAT: "Pode ter entre 2 e 10 dígitos.",
+  },
+  { id: 26, nome: "Suécia", codigoISOAlpha2: "SE", pattern: "[0-9]{12}", exampleVAT: "123456789012", descricaoExampleVAT: "Tem de ter 12 dígitos." },
+  { id: 27, nome: "Alemanha", codigoISOAlpha2: "DE", pattern: "[0-9]{9}", exampleVAT: "123456789", descricaoExampleVAT: "Tem de ter 9 dígitos." },
 ]
 
 export default function CountrySelector() {
   const [selectedCountry, setSelectedCountry] = useState(null)
   const [searchQuery, setSearchQuery] = useState("")
-  const [filteredCountries, setFilteredCountries] = useState(europeanCountrys)
+  const [filteredCountries, setFilteredCountries] = useState([])
   const [showDropdown, setShowDropdown] = useState(false)
   const [highlightedIndex, setHighlightedIndex] = useState(-1)
-  const [pattern, setPattern] = useState("") //estado para o respetivo sistema de identificação do país que for selecionado pelo user
+  const [exampleVAT, setExampleVAT] = useState("")
   const dropdownRef = useRef(null)
+  const inputRef = useRef(null)
 
   useEffect(() => {
     const normalizedQuery = normalizeString(searchQuery)
@@ -49,11 +127,11 @@ export default function CountrySelector() {
       (country) => normalizeString(country.nome).includes(normalizedQuery) || normalizeString(country.codigoISOAlpha2).includes(normalizedQuery)
     )
     setFilteredCountries(filtered)
-    setShowDropdown(normalizedQuery.length > 0 && filtered.length > 0)
+
+    //mostro dropdown se houver consulta e se houver resultados filtrados
+    setShowDropdown(searchQuery.length > 0)
   }, [searchQuery])
 
-  // função para normalizar strings, removendo acentos e convertendo para minúsculas
-  //'Croácia' não estava a ser aceite caso escreve-se 'Croacia' nem 'croácia'
   const normalizeString = (str) => {
     return str
       .normalize("NFD")
@@ -63,12 +141,24 @@ export default function CountrySelector() {
 
   const handleInputChange = (event) => {
     const query = event.target.value
-    setSearchQuery(query) //atualizo a query de pesquisa
-    setHighlightedIndex(-1) //reset o índice destacado
+    setSearchQuery(query)
+    setHighlightedIndex(-1)
+
+    //limpo seleção de país e exemplo de VAT se o input for modificado e não corresponder ao país selecionado
     if (selectedCountry && query !== `${selectedCountry.codigoISOAlpha2} | ${selectedCountry.nome}`) {
-      setSelectedCountry(null) //limpo a seleção se o input foi modificado
-      setPattern("") //limpo o padrão se a seleção for removida
+      setSelectedCountry(null)
+      setExampleVAT("")
     }
+
+    // 'Croácia' estava a ser diferente de 'Croacia' (sem acento) ou 'croacia'/'croácia' letras maisculas
+    const normalizedQuery = normalizeString(query)
+    const filtered = europeanCountrys.filter(
+      (country) => normalizeString(country.nome).includes(normalizedQuery) || normalizeString(country.codigoISOAlpha2).includes(normalizedQuery)
+    )
+
+    setFilteredCountries(filtered)
+    //mostro dropdown se houver texto e se houver resultados filtrados
+    setShowDropdown(query.length > 0)
   }
 
   const handleKeyDown = (event) => {
@@ -91,10 +181,10 @@ export default function CountrySelector() {
 
   const handleCountrySelect = (country) => {
     setSelectedCountry(country)
-    setSearchQuery(`${country.codigoISOAlpha2} | ${country.nome}`) //mostro o código e o nome ao selecionar
-    setPattern(country.pattern) // atualizo o padrão do país selecionado
-    setShowDropdown(false) //fecho o dropdown
-    setHighlightedIndex(-1) //faço reset  ao índice destacado
+    setSearchQuery(`${country.codigoISOAlpha2} | ${country.nome}`)
+    setExampleVAT(country.exampleVAT)
+    setShowDropdown(false)
+    setHighlightedIndex(-1)
   }
 
   const scrollToItem = (index) => {
@@ -121,14 +211,18 @@ export default function CountrySelector() {
 
   const handleBlur = () => {
     setTimeout(() => {
-      if (!dropdownRef.current?.contains(document.activeElement)) {
+      if (!dropdownRef.current?.contains(document.activeElement) && !inputRef.current?.contains(document.activeElement)) {
         setShowDropdown(false)
+        //se um país estiver selecionado, não limpo o campo de pesquisa
         if (!selectedCountry) {
-          //atualizo o searchQuery apenas se nenhum país estiver selecionado
           setSearchQuery("")
         }
       }
-    }, 100)
+    }, 150)
+  }
+
+  const handleMouseDown = (event) => {
+    event.preventDefault()
   }
 
   const styles = {
@@ -185,6 +279,7 @@ export default function CountrySelector() {
         <div style={styles.countrySelector}>
           <label className="fs-5 fw-bold mb-2">País</label>
           <input
+            ref={inputRef}
             type="text"
             value={searchQuery}
             placeholder="Introduza o país ou código"
@@ -195,7 +290,7 @@ export default function CountrySelector() {
             style={styles.input}
           />
           {showDropdown && (
-            <div style={styles.dropdown} ref={dropdownRef}>
+            <div style={styles.dropdown} ref={dropdownRef} onMouseDown={handleMouseDown}>
               {filteredCountries.length > 0
                 ? filteredCountries.map((country, index) => (
                     <div
@@ -209,13 +304,14 @@ export default function CountrySelector() {
                       {country.codigoISOAlpha2} | {country.nome}
                     </div>
                   ))
-                : searchQuery && <div style={styles.noResults}>Nenhum resultado encontrado</div>}
+                : //mostro a mensagem apenas se não houver países filtrados e o campo de pesquisa não corresponder ao país selecionado
+                  searchQuery.length > 0 && !selectedCountry && <div style={styles.noResults}>Nenhum resultado encontrado</div>}
             </div>
           )}
         </div>
         <div>
           <label className="fs-5 fw-bold mb-2">NIF / VAT</label>
-          <input type="text" placeholder={pattern ? `Padrão: ${pattern}` : "Introduza o NIF / VAT"} style={styles.input} disabled />
+          <input type="text" placeholder={exampleVAT ? `${exampleVAT}` : "Introduza o NIF / VAT"} style={styles.input} disabled />
         </div>
       </div>
     </div>
